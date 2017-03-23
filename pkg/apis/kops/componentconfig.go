44c2836b5a8331dfc4ec942d0de0754770785f72
Abrar Shivani <shivania@vmware.com>
2017-03-23 00:15:31 -0700

    Support for vSphere Cloud Provider

--- pkg/apis/kops/componentconfig.go
+++ pkg/apis/kops/componentconfig.go
@@ -691,8 +691,11 @@ type CloudConfiguration struct {
 	NodeInstancePrefix *string `json:"nodeInstancePrefix,omitempty"`
 
 	// vSphere cloud-config specs
+	VSphereUsername      *string `json:"vSphereUsername,omitempty"`
+	VSpherePassword      *string `json:"vSpherePassword,omitempty"`
 	VSphereServer        *string `json:"vSphereServer,omitempty"`
 	VSphereDatacenter    *string `json:"vSphereDatacenter,omitempty"`
 	VSphereResourcePool  *string `json:"vSphereResourcePool,omitempty"`
+	VSphereDatastore     *string `json:"vSphereDatastore,omitempty"`
 	VSphereCoreDNSServer *string `json:"vSphereCoreDNSServer,omitempty"`
 }
